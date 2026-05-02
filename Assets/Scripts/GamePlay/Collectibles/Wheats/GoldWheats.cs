e7c34ed9a53e89bc0494caf8afd334f150518e1d
Omega351 <burucewayne339@gmail.com>
2026-05-02 17:02:37 +0300

    Collectible Interface added

--- Assets/Scripts/GamePlay/Collectibles/Wheats/GoldWheats.cs
+++ Assets/Scripts/GamePlay/Collectibles/Wheats/GoldWheats.cs
@@ -1,11 +1,12 @@
 using System.Runtime.Serialization;
 using UnityEngine;
 
-public class GoldWheats : MonoBehaviour
+public class GoldWheats : MonoBehaviour , ICollectible
 {
    [SerializeField] private PlayerController playerController;
    [SerializeField] private float IncreaseMovementSpeed;
    [SerializeField] private float IncreaseTime;
+   
 
    public void Collect()
     {
--- Assets/Scripts/GamePlay/Player/InteractionController.cs
+++ Assets/Scripts/GamePlay/Player/InteractionController.cs
@@ -1,21 +1,14 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
 
 public class PlayerInteractionController : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag(Consts.WheatTypes.GOLD_WHEAT))
+        if(other.gameObject.TryGetComponent<ICollectible>(out var collectible) )
         {
-            other.gameObject.GetComponent<GoldWheats>().Collect();
-        }
-        if(other.CompareTag(Consts.WheatTypes.HOLY_WHEAT))
-        {
-            other.gameObject.GetComponent<HolyWheats>().Collect();
-        }
+            collectible.Collect();
 
-        if(other.CompareTag(Consts.WheatTypes.ROTTEN_WHEAT))
-        {
-            other.gameObject.GetComponent<RottenWheats>().Collect();
         }
     }
 }
